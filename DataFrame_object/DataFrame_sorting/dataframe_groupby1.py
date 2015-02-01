9975ebd31ac7ce3cab6be90c05c73aa1d754d003
liangk12 <kenneth.liang12@gmail.com>
2015-01-31 22:36:58 -0500

    working on plotting DataFrame groupby grouped objs; including a guide on getting matplotlib installed - working on it

---
+++ DataFrame_object/DataFrame_sorting/dataframe_groupby1.py
@@ -0,0 +1,53 @@
+import pandas
+
+"""
+'GroupBy' : splitting --> analyzing --> combining
+ 
+Splitting:
+ grouped obj : group of DataFrame objs in a dict-like structure
+
+ ~~~ DataFrame --groupby--> grouped obj of DataFrames
+
+Analyzing:
+ can analyze the group of DataFrame objs
+
+Combining:
+ creating new data structures 
+"""
+
+mlb = pandas.read_csv("mlbsalaries.csv") # whole DataFrame
+# print mlb # [19543 rows x 5 columns]
+# print mlb.head()
+# print mlb.tails()
+
+### # Splitting
+# dict of data by key "Year"
+grpMlb = mlb.groupby("Year")
+# print grpMlb # <pandas.core.groupby.DataFrameGroupBy object...>
+
+'''
+for k, group in grpMlb:
+    print k
+    print group
+'''
+
+### # Analysis
+# generator comprehension
+# each group -> sort by salary -> highest at top (ascending=False)
+t = (group.sort_index(by="Salary", ascending=False)[:1] for yr, group in grpMlb)
+# print t # <generator object <genexpr>...>
+
+### # Combining
+topsalaries = pandas.DataFrame()
+for row in t:
+    topsalaries = topsalaries.append(row)
+# print topsalaries
+# # sets index col to "Year", remove the left side of row index nums from list
+# topsalaries = topsalaries.set_index(["Year"])
+# print topsalaries
+
+ts = topsalaries[["Year","Salary"]]
+# print ts
+ts = ts.set_index("Year")
+# print ts
+# ts.plot() # need to install matplotlib
