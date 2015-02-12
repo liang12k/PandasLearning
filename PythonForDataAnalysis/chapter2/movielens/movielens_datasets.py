40dd86bb0cf156e7f848ad5c2eb1de98bf9d6727
liangk12 <kenneth.liang12@gmail.com>
2015-02-12 00:04:59 -0500

    finished the movielens dataset - working on groupby, used variance and std deviation; measure variance by the largest difference

--- PythonForDataAnalysis/chapter2/movielens/movielens_datasets.py
+++ PythonForDataAnalysis/chapter2/movielens/movielens_datasets.py
@@ -62,3 +62,31 @@ mean_ratings = mean_ratings.ix[active_titles]
 # # filter films based on females; sort by F col
 top_female_ratings = mean_ratings.sort_index(by="F", ascending=False)
 # print top_female_ratings[:10]
+
+# # measuring rating disagreement
+# # appending "diff" col; sort by "diff" col
+mean_ratings["diff"] = mean_ratings["M"] - mean_ratings["F"]
+sorted_by_diff = mean_ratings.sort_index(by="diff")
+# print sorted_by_diff[:15]
+# # reversing row order using list comprehension [::-1]
+# print sorted_by_diff[::-1][:15]
+
+# # getting largest disagreement by variance
+# # std deviation of rating grouped by title
+# # Reminder: data is merging of 3 DataFrames
+rating_std_by_title = data.groupby("title")["rating"].std()
+# # filter down to active_titles
+rating_std_by_title = rating_std_by_title.ix[active_titles]
+# print rating_std_by_title.order(ascending=False)[:10]
+
+### # variance: 
+### # ---------
+### # http://stattrek.com/statistics/dictionary.aspx?definition=variance
+### # -numerical value indicate wide difference by group
+### # -distinguish between variance of population & variance of sample
+### # 
+### # http://simple.wikipedia.org/wiki/Variance
+### # -in probability theory, statistics measure how far set of numbers is spread out
+### # -measure how much something changes
+
+### # TODO: will work on dataset to analyze by genre
