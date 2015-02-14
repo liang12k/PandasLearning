91425f9c7193cca4d615b7cc43d940c19f858f23
liangk12 <kenneth.liang12@gmail.com>
2015-02-13 23:24:41 -0500

    continuing working on Ch2 - working with groupby, col filtering, plotting; getting data subsets, analyzing name trends

--- PythonForDataAnalysis/chapter2/usbabynames/babynames_dataset.py
+++ PythonForDataAnalysis/chapter2/usbabynames/babynames_dataset.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pylab
+import numpy as np
 
 names1880 = pd.read_csv("names/yob1880.txt", names=["name","sex","births"])
 # print names1880 # [2000 rows x 3 columns]
@@ -30,3 +31,59 @@ total_births = names.pivot_table(
 )
 # print total_births.tail()
 # total_births.plot(title="Total births by sex and year"); pylab.show()
+
+# # insert col "prop" == babies for each name / total number of births
+# # prop val = 0.2; 2/100 babies w particular name
+# # --group data by year & sex --> add new col to each group
+def add_prop(group):
+    births = group.births.astype(float) # int division floors
+    group["prop"]=(births/births.sum()) # births of this group / total births
+    return group
+names = names.groupby(["year","sex"]).apply(add_prop)
+# print names # [1690784 rows x 5 columns]
+
+# # self-code: see top,bottom 5 of "names" DataFrame 
+# #            based on gender, sorted by "prop" col
+# m_names = names[names.sex=="M"]
+# f_names = names[names.sex=="F"]
+# gethead_prop = lambda dF: dF.sort(columns=["prop"]).head()
+# gettail_prop = lambda dF: dF.sort(columns=["prop"]).tail()
+# 
+# print names[names.sex=="M"].sort(columns=["prop"]).head()
+# print names[names.sex=="M"].sort(columns=["prop"]).tail()
+# print names[names.sex=="F"].sort(columns=["prop"]).head()
+# print names[names.sex=="F"].sort(columns=["prop"]).tail()
+# # 
+
+# # sanity check (verify "prop" col sum == 1 within all groups)
+# # np.allclose check that group sums ~= 1 (doesn't need to be exact!)
+# print np.allclose(names.groupby(["year","sex"]).prop.sum(), 1) # True
+
+# # extract data subset to analyze top 1000 names for each
+# # (sex / year) combination using group operation
+# # ::: separate DataFrames by "year","sex"; ordered by "births" count
+def get_top1000(group):
+    return group.sort_index(by="births",ascending=False)[:1000]
+grouped = names.groupby(["year","sex"])
+top1000 = grouped.apply(get_top1000)
+# print top1000
+
+# # working w top 1000 names dataset
+# # pivot_table made DataFrame have:
+# # :index = "year" (per row)
+# # :columns = "names"
+# # :values = "births" count per name in year 
+boys = top1000[top1000.sex=="M"]
+girls = top1000[top1000.sex=="F"]
+total_births = top1000.pivot_table(
+    "births", index="year", columns="name", aggfunc="sum"
+)# using args "index","columns" because "rows","cols" are deprecated
+# print total_births
+
+# # taking multiple subset of 4 colnames
+# # plots count of 4 names in births in years range
+subset = total_births[["John","Harry","Mary","Marilyn"]]
+# subset.plot(
+#     subplots=True, figsize=(12,10), grid=False, 
+#     title="Number of births per year"
+# ); pylab.show()
