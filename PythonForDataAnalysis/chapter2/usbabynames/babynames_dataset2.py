64cdd9d3329a8e91b0871ab78cb497ef01cc09a1
liangk12 <kenneth.liang12@gmail.com>
2015-02-14 22:55:56 -0500

    putting print statements to see colnames, index name; trying to understand pd.Series.searchsorted method

--- PythonForDataAnalysis/chapter2/usbabynames/babynames_dataset2.py
+++ PythonForDataAnalysis/chapter2/usbabynames/babynames_dataset2.py
@@ -36,16 +36,25 @@ table.plot(
 
 # # number of distinct names, high-to-low popularity
 # # in top 50% births
-boysdf = boys[boys.year==2010]
-# print boysdf # [1000 rows x 5 columns] # sorted by .prop
-# print boysdf.sort_index()[:20]
+boys2010df = boys[boys.year==2010]
+# print boys2010df # [1000 rows x 5 columns] # sorted by .prop
+# print boys2010df.sort_index()[:20]
 # # Index([u'name', u'sex', u'births', u'year', u'prop'], dtype='object')
-# print boysdf.columns
-# print boysdf.index.name # None # index not set yet
+# print boys2010df.columns
+# print boys2010df.index.name # None # index not set yet
 
 # # numpy : vectorized approach
 # # cumsum (cumulative sum) of "prop" col
 # # searchsorted : returns position in cumsum,
 # #                0.5 insert needed to keep in sorted order
-boysprop_cumsum = boysdf.sort_index(by="prop",ascending=False).prop.cumsum()
-print boysprop_cumsum[:10] # printed by index (idx not set),
+boys2010prop_cumsum = boys2010df.sort_index(by="prop",ascending=False).prop.cumsum()
+# # array is zero-indexed
+# print boysprop_cumsum[:10] # printed by index (idx not set),
+
+# # zero-indexed array, +1 to result gives 117
+boys1900df = boys[boys.year==1900]
+in1900 = boys1900df.sort_index(by="prop",ascending=False).prop.cumsum()
+# # find indices where elements should be inserted to maintain order
+# # :return indices: array of ints
+# print in1900.searchsorted(0.5)+1 # [25]
+# print type(in1900) # <class 'pandas.core.series.Series'>
