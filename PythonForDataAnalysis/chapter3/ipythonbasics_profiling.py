9ebc1a13caa45a4c29a0d37d896d639cff574a70
liangk12 <kenneth.liang12@gmail.com>
2015-02-22 16:12:32 -0500

    continuing profiling code using %prun; observing -s sortkey (chaining availability). Ref link: ipython IPython.core.magics.execution

--- PythonForDataAnalysis/chapter3/ipythonbasics_profiling.py
+++ PythonForDataAnalysis/chapter3/ipythonbasics_profiling.py
@@ -4,9 +4,21 @@ from numpy.linalg import eigvals
 """
 profiling code == determining where time is spent
 
+in python-
 cProfile: executes program, arbitrary code block
           while keeping track of time spent
           in each method
+
+------------------------
+generally-
+%prun _method_()
+
+using output 'filtering'
+: <#>: replace w a number, total lines output displayed
+: <s_key>: sort by key, 
+: ref: http://ipython.org/ipython-doc/2/api/generated/IPython.core.magics.execution.html
+:          Note: can chain sorts by -s ___ -s ___ ...
+%prun -l <#> -s <s_key> _method_()
 """
 
 def runLoop_linearAlgebra(niter=100):
@@ -23,3 +35,23 @@ print ("Largest one we saw was: %s"
 
 # # %run ipythonbasics_profiling.py
 # Largest one we saw was: 11.4336167842
+#
+
+# %prun ipythonbasics_profiling.py
+# 
+# similar to terminal cmd (reg. shell):
+# >>> python -m cProfile -s cumulative ipythonbasics_profiling.py
+# 
+# # %prun -l 7 -s cumulative ipythonbasics_profiling.py
+# # -l : set a limit of output
+# # -s : sort profile by given key
+# # cumulative : key, cumulative time
+#
+# 
+# 4003 function calls in 1.367 seconds
+# 
+#    Ordered by: cumulative time
+#    List reduced from 32 to 7 due to restriction
+# 
+
+
