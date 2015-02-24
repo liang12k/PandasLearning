74c6226e903be958032e16340c4d90d89db8eec3
liangk12 <kenneth.liang12@gmail.com>
2015-02-23 23:54:29 -0500

    beginners steps on scalar operations on np.array obj

--- PythonForDataAnalysis/chapter4/__init__.py
+++ PythonForDataAnalysis/chapter4/__init__.py
@@ -3,4 +3,17 @@ Chapter4:
 ---------
 
 NumPy Basics: Array, Vectorized computation
+
+standard syntax:
+import numpy as np
+
+** tools for integrating code written in C, C++, Fortran
+
+ ^ from ecosystem POV, NumPy provide easy-to-use C API
+easy to pass data to external libs written in low-level
+language & external libs to return data to Python as
+NumPy arrays.
+
+::: therefore, Python wraps legacy C/C++/Fortran
+codebases - giving them a dynamic, easy-to-use interface
 """
---
+++ PythonForDataAnalysis/chapter4/ndarray_multidimarrayobj.py
@@ -0,0 +1,26 @@
+"""
+ndarray : N-dimensional array obj
+-fast, flexible container for large data sets in Python
+
+arrays: perform mathematical operations on whole blocks
+of data using similar syntax to equivalent operations 
+between scalar elements
+
+scalar: simple numbers for measuring things
+"""
+
+import numpy as np
+
+data = np.array([
+    [0.9256,-0.246,-0.8856],
+    [0.5639,2.3794,9.104]
+])
+
+# data * 10
+# array([[  9.256,  -2.46 ,  -8.856],
+#       [  5.639,  23.794,  91.04 ]])
+
+# data + data
+# array([[  1.8512,  -0.492 ,  -1.7712],
+#       [  1.1278,   4.7588,  18.208 ]])
+
