9ab0e189beb0b94c0bda61e286dc4f17cb9d07d8
liangk12 <kenneth.liang12@gmail.com>
2015-02-28 17:48:43 -0500

    move ndarray_ named modules in ndarray dir. Placing ufunc into universalfunction dir and working on ufunc_examples. Organizing the modules

---
+++ PythonForDataAnalysis/chapter4/universalfunctions/ufunc_examples.py
@@ -0,0 +1,19 @@
+"""
+
+"""
+
+import numpy as np
+
+arr=np.arange(10)
+# # np.sqrt is a ufunc
+# # takes scalar values in ndarray and
+# # produces scalar values
+np.sqrt(arr)
+# # note: arr is a 1D array
+# array(
+#     [ 0.        ,  1.        ,  1.41421356,
+#       1.73205081,  2.        ,  2.23606798,
+#       2.44948974,  2.64575131,  2.82842712,  3.
+#     ]
+# )
+
