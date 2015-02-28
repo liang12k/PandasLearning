5506ef8045f27e6b5dcb9d99cee77fc344d1717e
liangk12 <kenneth.liang12@gmail.com>
2015-02-28 17:59:55 -0500

    working on ufunc example, learning unary,binary ufuncs. Trying to understand np.modf

--- PythonForDataAnalysis/chapter4/universalfunctions/ufunc_examples.py
+++ PythonForDataAnalysis/chapter4/universalfunctions/ufunc_examples.py
@@ -8,6 +8,11 @@ arr=np.arange(10)
 # # np.sqrt is a ufunc
 # # takes scalar values in ndarray and
 # # produces scalar values
+
+#
+# # unary ufuncs: np.sqrt, np.exp
+# # ^ math operations involving single element
+
 np.sqrt(arr)
 # # note: arr is a 1D array
 # array(
@@ -17,3 +22,27 @@ np.sqrt(arr)
 #     ]
 # )
 
+np.exp(arr)
+# # note: arr is a 1D array
+# array(
+#     [  1.00000000e+00,   2.71828183e+00,
+#        7.38905610e+00,   2.00855369e+01,
+#        5.45981500e+01,   1.48413159e+02,
+#        4.03428793e+02,   1.09663316e+03,
+#        2.98095799e+03,   8.10308393e+03
+#    ])
+
+#
+# # binary ufuncs: np.maxinum
+# # ^ involving 2 elements
+#
+
+x = np.random.randn(8)
+y = np.random.randn(8)
+# element-wise operation, taking max value
+# betw x,y per index
+np.maximum(x,y) 
+
+#
+# # ufunc returning multiple arrays
+# # ex: np.modf 
