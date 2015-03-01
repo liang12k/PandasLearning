1eb81a9c1dd8d06ebdff892051a7fae3b37ad9f4
liangk12 <kenneth.liang12@gmail.com>
2015-02-28 22:31:43 -0500

    working on ufunc section - comments for the Table4-3,4-4 as references. Methods operate on same .shape arrays (broadcasting)

--- PythonForDataAnalysis/chapter4/universalfunctions/ufunc_examples.py
+++ PythonForDataAnalysis/chapter4/universalfunctions/ufunc_examples.py
@@ -1,4 +1,4 @@
-"""
+1"""
 
 """
 
@@ -94,3 +94,71 @@ np.sign(arr2) # sign of each element (pos=1,zero=0,neg=-1)
 # array([ 1., -1., -1.,  1., -1.])
 
 np.ceil(arr2) # get ceil of scalar elements
+              # the smallest int value >= element
+# array([    3.,    -9.,    -0.,    51., -1947.]
+
+np.floor(arr2) # get floor of scalar elements
+               # the largest int value <= element
+# array(
+#      [ 2.00000000e+00,
+#        -1.00000000e+01,
+#        -1.00000000e+00,
+#        5.00000000e+01,
+#        -1.94800000e+03]
+# )
+
+np.rint(arr2) # round elements to nearest int,
+              # perserving dtype
+# array([    2.,    -9.,    -0.,    50., -1948.])
+
+np.isnan(arr2) # get bool array indicating each
+               # value == NaN (not-a-number)
+
+np.isfinite # each element is finite (non-inf, non-NaA)
+np.isinf # each element is infinite
+
+# trigonometric functions & hyperbolic alternative
+# inverse trigonometric functions
+np.cos, np.cosh, np.arccos, np.arccosh
+np.sin, np.sinh, np.arcsin, np.arcsinh
+np.tan, np.tanh, np.arctan, np.arctanh
+
+# logic applied to elements
+np.logical_and, np.logical_or, np.logical_not
+np.logical_xor # element satisfy ONLY one of critera
+               # arr1, arr2 must be same shape
+# ^ http://goo.gl/0k8lF2
+# Joe==male,25, Bob==male,30, Anne==female,25
+# "male AND 25"==Joe
+# "male OR 25"==Joe,Bob,Anne (**OR condition)
+# "male XOR 25"==Bob,Anne (only meet 1 condition)
+
+# 
+# # binary ufunc
+#
+np.add # add corresponding elements in x1,x2 arrays
+np.subtract # '' similar, subtrace
+np.multiply # '' similar, multiple
+np.divide   # '' similar, divide
+np.floor_divide # '' similar, remove remainder
+
+np.power # x1 array elements raised to x2 values
+         # x2's values are x1 elements' exponents
+# np.power(np.arange(4),np.array([5,3,4,2]))
+# array([ 0,  1, 16,  9]) # [0^5, 1^3, 2^4, 3^2]
+
+np.maximum # output array w max vals of
+           # each index from each array
+np.minimum # '' similar, minimum
+np.fmax    # similar to np.maximum, ignore NaN vals
+np.fmin    # similar to np.minimum, min
+
+np.mod     # '' similar, mod operation
+np.copysign# '' similar, copy sign of x2 into x1
+
+# # greater, less, equal assorted
+# # x2 values compared to x1 values
+np.greater, np.greater_equal
+np.less,    np.less_equal
+np.equal,   np.not_equal
+
