dc1f7e4ff3a41a5c3ded1a8fb7a782e6908cd0ba
liangk12 <kenneth.liang12@gmail.com>
2015-03-02 21:30:05 -0500

    went thru basic save,load binary and txt formats. Self-code to append a row then write and read it

---
+++ PythonForDataAnalysis/chapter4/dataprocessing/fileio_witharrays/saveloading_textfiles/saveloadtext_examples.py
@@ -0,0 +1,22 @@
+"""
+np.loadtxt: load data into vanilla numpy arrays
+
+np.genfromtxt: specialized load of np.loadtxt
+               **more for structured arrays
+"""
+
+import numpy as np
+
+arr=np.loadtxt("array_ex.txt",delimiter=",")
+# array([
+#     [ 0.580052,  0.18673 ,  1.040717,  1.134411],
+#     [ 0.194163, -0.636917, -0.938659,  0.124094],
+#     [-0.12641 ,  0.268607, -0.695724,  0.047428],
+#     [-1.484413,  0.004176, -0.744203,  0.005487],
+#     [ 2.302869,  0.200131,  1.670238, -1.88109 ],
+#     [-0.19323 ,  1.047233,  0.482803,  0.960334]
+# ])
+
+newarr=np.append(arr,[11.,22.,33.,44.,55.])
+np.savetxt("array_ex_appendrow.txt",newarr)
+np.loadtxt("array_ex_appendrow.txt")
