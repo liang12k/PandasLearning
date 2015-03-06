aeaacaba94fdfaa030c811fb9e4fc16cb210a0a6
liangk12 <kenneth.liang12@gmail.com>
2015-03-06 00:09:25 -0500

    went thru random walks (pt1). Working on 'Simulating Many Random Walks at Once' section (pt2)

--- PythonForDataAnalysis/chapter4/randomnumbergeneration/randomwalks/randomwalks_exmaple.py
+++ PythonForDataAnalysis/chapter4/randomnumbergeneration/randomwalks/randomwalks_exmaple.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 # random walk starting at 0,
 # steps of 1,-1 w equal probability
-#
+
 # thru python
 import random
 position=0
@@ -18,4 +18,44 @@ for i in xrange(steps):
     step=1 if random.randint(0,1) else -1
     position += step
     walk.append(position)
+
+# a=np.array(walk)
+# plt.plot(a)
+# plt.title("Random walk w +1/-1 steps")
+# plt.savefig("ch4_randwalks")
+# plt.show();
+
+nsteps=1000
+draws=np.random.randint(0,2,size=nsteps) # [0,2)
+steps=np.where(draws>0,1,-1) # (cond, if-True, else)
+walk=steps.cumsum() # array of steps by randn
+# plt.plot(walk)
+# plt.title("Random walk w +1/-1 steps")
+# plt.savefig("ch4_randwalks_viaNumpy")
+# plt.show()
+walk.min(), walk.max()
+
+# # first crossing time
+# # -step when rand walk reaches particular value
+#
+# how long it took rand walk to get at least
+# 10 steps away from origin 0 in either direction
+# -abs (absolute value) as direction is negligible
+# 
+# bool array where walk exceeded 10
+np.abs(walk)>=10
+# specifically want to know index of the
+# first occurence 10, -10 (either direction)
+# -note: inefficient as it may scan entire array
+(np.abs(walk)>=10).argmax()
+
+# np.argmax : returns 1D array of indicies of max
+#             arg; can specify axis to examine
+# ex:
+# arr=np.arange(12).reshape(4,3)
+# np.argmax(arr) # 11
+# # axis=0: cmp cols - only 3 cols
+# np.argmax(arr,axis=0) # array([3, 3, 3])
+# # axis=1: cmp rows - only 4 rows
+# np.argmax(arr,axis=1) # array([2, 2, 2, 2])
 
