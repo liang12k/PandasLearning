51795257d8222400d070e29df90aa4b6c4a68123
liangk12 <kenneth.liang12@gmail.com>
2015-03-06 21:48:10 -0500

    working on ch5 - intro to pandas. Beginning on Series examples

---
+++ PythonForDataAnalysis/chapter5/pandasintro/datastructures/series_examples.py
@@ -0,0 +1,15 @@
+"""
+series: 1D array-like obj containing array of data
+        and an index (data labels)
+"""
+
+import pandas as pd
+
+# simplest Series formed
+obj=pd.Series([4,7,-5,3])
+# 0    4
+# 1    7
+# 2   -5
+# 3    3
+# dtype: int64
+
