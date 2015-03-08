28bd92162b82e576be03479a3202e07b3c64f12e
liangk12 <kenneth.liang12@gmail.com>
2015-03-07 19:51:31 -0500

    cont'd working on ch5 DataFrame section - insert cols, creating dataframes, editing values, filtering based on val. Expanding,Limiting dataframe

--- PythonForDataAnalysis/chapter5/pandasintro/datastructures/dataframe_examples.py
+++ PythonForDataAnalysis/chapter5/pandasintro/datastructures/dataframe_examples.py
@@ -19,11 +19,128 @@ data={
 }
 frame=pd.DataFrame(data)
 frame
-   # pop   state  year
+   #    pop   state  year
    # 0  1.5    Ohio  2000
    # 1  1.7    Ohio  2001
    # 2  3.6    Ohio  2002
    # 3  2.4  Nevada  2001
    # 4  2.9  Nevada  2002
 
-   
+# # specifying sequence of cols (project cols)
+pd.DataFrame(data,columns=["year","state","pop"])
+   #    year   state  pop
+   # 0  2000    Ohio  1.5
+   # 1  2001    Ohio  1.7
+   # 2  2002    Ohio  3.6
+   # 3  2001  Nevada  2.4
+   # 4  2002  Nevada  2.9
+
+# # projecting cols that DNE (don't exist)
+# # -get NaN vals in cols, no error
+frame2=pd.DataFrame(
+    data,
+    columns=["year","state","pop","debt"],
+    index=["one","two","three","four","five"]
+)
+frame2
+   #        year   state  pop debt
+   # one    2000    Ohio  1.5  NaN
+   # two    2001    Ohio  1.7  NaN
+   # three  2002    Ohio  3.6  NaN
+   # four   2001  Nevada  2.4  NaN
+   # five   2002  Nevada  2.9  NaN
+frame2.columns
+# Index([u'year', u'state', u'pop', u'debt'], dtype='object')
+#
+# # selecting specific col name
+# # either by df["_col_name_"] or df.colname
+# # -note: same indices are used w projections
+frame2["state"]
+# one        Ohio
+# two        Ohio
+# three      Ohio
+# four     Nevada
+# five     Nevada
+# Name: state, dtype: object
+frame2.year
+# one      2000
+# two      2001
+# three    2002
+# four     2001
+# five     2002
+# Name: year, dtype: int64
+#
+# # selecting index name
+frame2.ix["three"]
+# year     2002
+# state    Ohio
+# pop       3.6
+# debt      NaN
+# Name: three, dtype: object
+#
+# # filtering rows
+frame2[
+    (frame2.year>2001) &
+    (frame2.state=="Ohio")
+]
+#        year state  pop debt
+# three  2002  Ohio  3.6  NaN
+#
+# # modifying cols by assignment
+#
+# constant scalar value
+frame2["debt"]=16.5
+frame2
+  #        year   state  pop  debt
+  # one    2000    Ohio  1.5  16.5
+  # two    2001    Ohio  1.7  16.5
+  # three  2002    Ohio  3.6  16.5
+  # four   2001  Nevada  2.4  16.5
+  # five   2002  Nevada  2.9  16.5
+#
+# assigning array to col
+# -note: must be same length!
+frame2["debt"]=list(range(10,15))
+frame2
+  #        year   state  pop  debt
+  # one    2000    Ohio  1.5    10
+  # two    2001    Ohio  1.7    11
+  # three  2002    Ohio  3.6    12
+  # four   2001  Nevada  2.4    13
+  # five   2002  Nevada  2.9    14
+frame2["debt"]=np.arange(5.)
+frame2
+ #        year   state  pop  debt
+ # one    2000    Ohio  1.5     0
+ # two    2001    Ohio  1.7     1
+ # three  2002    Ohio  3.6     2
+ # four   2001  Nevada  2.4     3
+ # five   2002  Nevada  2.9     4
+
+# # assigning Series to col
+# # -note: matches vals based on
+# #        Series index <-> frame2 index
+val=pd.Series(
+    [-1.2,-1.5,-1.7],
+    index=["two","four","five"]
+)
+frame2["debt"]=val
+frame2
+ #        year   state  pop  debt
+ # one    2000    Ohio  1.5   NaN
+ # two    2001    Ohio  1.7  -1.2
+ # three  2002    Ohio  3.6   NaN
+ # four   2001  Nevada  2.4  -1.5
+ # five   2002  Nevada  2.9  -1.7
+#
+# # assigning new col that DNE creates that new col
+frame2["eastern"]=(frame2.state=="Ohio")
+frame2
+ #        year   state  pop  debt eastern
+ # one    2000    Ohio  1.5   NaN    True
+ # two    2001    Ohio  1.7  -1.2    True
+ # three  2002    Ohio  3.6   NaN    True
+ # four   2001  Nevada  2.4  -1.5   False
+ # five   2002  Nevada  2.9  -1.7   False
+#
+ 
