9dcc068f63b0d31581c939d05e7b0559ac291c15
liangk12 <kenneth.liang12@gmail.com>
2015-03-07 22:11:45 -0500

    placeholder for indexobjects section

---
+++ PythonForDataAnalysis/chapter5/pandasintro/datastructures/indexobjects.py
@@ -0,0 +1,12 @@
+"""
+pandas's Index objects hold axis lates and
+other metadata (ie: axis names)
+-array or other sequence of labels used when
+ constructing Series, DataFrame internally
+ converted to an Index
+
+"""
+
+import pandas as pd
+import numpy as np
+
