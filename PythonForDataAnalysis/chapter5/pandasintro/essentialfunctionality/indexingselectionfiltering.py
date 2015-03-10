6a80c159aea787d3612bc69a8c0f6126feed713c
liangk12 <kenneth.liang12@gmail.com>
2015-03-10 00:27:16 -0400

    went thru indexing,selecting,filtering section - wrangled table5-6 into a dict

--- PythonForDataAnalysis/chapter5/pandasintro/essentialfunctionality/indexingselectionfiltering.py
+++ PythonForDataAnalysis/chapter5/pandasintro/essentialfunctionality/indexingselectionfiltering.py
@@ -1,5 +1,7 @@
 """
-
+.ix - DataFrame label-indexing
+      select subset of rows,cols from DataFrame
+      with NumPy like notation with axis labels
 """
 
 import numpy as np
@@ -99,3 +101,37 @@ data[data<5]=0 # assigning 0 to all True vals
 # Utah        8    9     10    11
 # New York   12   13     14    15
 
+# # dataframe .ix
+data.ix["Colorado",["two","four"]]
+# two     5
+# four    7
+# Name: Colorado, dtype: int64
+#
+# select row names, col indices
+data.ix[["Colorado","Utah"],[3,0,1]]
+#           four  one  two
+# Colorado     7    0    5
+# Utah        11    8    9
+#
+# select col "two" w row values up to "Utah"
+# Ohio        0
+# Colorado    5
+# Utah        9
+# Name: two, dtype: int64
+#
+# "three" col values > 5, up to 2 col index
+data.ix[data.three>5, :3]
+#           one  two  three
+# Colorado    0    5      6
+# Utah        8    9     10
+# New York   12   13     14
+
+# # Table5-6
+{'get_value, set_value methods': 'Select single value by row and column label.',
+ 'icol, irowmethods': 'Select single column or row, respectively, as a Series by integer location.',
+ 'obj.ix[:, val]': 'Selects single column of subset of columns.',
+ 'obj.ix[val1, val2]': 'Select both rows and columns.',
+ 'obj.ix[val]': 'Selects single row of subset of rows from the DataFrame.',
+ 'obj[val]': 'Select single column or sequence of columns from the DataFrame. Special case con- veniences: boolean array (filter rows), slice (slice rows), or boolean DataFrame (set values based on some criterion).',
+ 'reindex method': 'Conform one or more axes to new indexes.',
+ 'xs method': 'Select single row or column as a Series by label.'}
