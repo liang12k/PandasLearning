b4fd09c22c99eb35f58885818b267b4dfb22eeb5
liangk12 <kenneth.liang12@gmail.com>
2015-03-14 22:43:51 -0400

    went thru axis and duplicating index values section - used pd.Series.index.is_unique, pd.DataFrame.index.is_unique

--- PythonForDataAnalysis/chapter5/pandasintro/essentialfunctionality/axisindexesduplicatevalues.py
+++ PythonForDataAnalysis/chapter5/pandasintro/essentialfunctionality/axisindexesduplicatevalues.py
@@ -5,3 +5,40 @@
 import numpy as np
 import pandas as pd
 
+obj=pd.Series(
+    range(5),
+    index=list("aabbc")
+)
+obj
+# a    0
+# a    1
+# b    2
+# b    3
+# c    4
+# dtype: int64
+obj.index.is_unique
+# False
+# http://pandas.pydata.org/pandas-docs/dev/generated/pandas.Index.html
+obj["a"]
+# a    0
+# a    1
+# dtype: int64
+obj["c"]
+# 4
+
+df=pd.DataFrame(
+    np.random.randn(4,3),
+    index=list("aabb")
+)
+df
+#           0         1         2
+# a -0.484903  1.393460  0.353473
+# a -0.732426 -1.658375 -0.101024
+# b  0.230659  0.230805 -1.237065
+# b -0.355484  1.186528  0.465216
+df.ix["b"]
+#           0         1         2
+# b  0.230659  0.230805 -1.237065
+# b -0.355484  1.186528  0.465216
+df.index.unique()
+# array(['a', 'b'], dtype=object)
