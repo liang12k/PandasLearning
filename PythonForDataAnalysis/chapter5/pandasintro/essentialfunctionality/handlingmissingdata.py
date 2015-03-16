aef03c289c6084096228f63101be8d5d2189730a
liangk12 <kenneth.liang12@gmail.com>
2015-03-15 23:59:37 -0400

    working on handling missing data section

---
+++ PythonForDataAnalysis/chapter5/pandasintro/essentialfunctionality/handlingmissingdata.py
@@ -0,0 +1,13 @@
+"""
+NaN: missing data default value
+     -a sentinel for easy detection
+     -aka: flag value, signal value
+     >sentinel: http://en.wikipedia.org/wiki/Sentinel_value
+     > http://www.webopedia.com/TERM/S/sentinel_value.html
+     : special value guarantees termination
+     : not a 'legitimate' value in loop
+"""
+
+import numpy as np
+import pandas as pd
+
