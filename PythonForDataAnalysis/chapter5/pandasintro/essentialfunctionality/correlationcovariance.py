2c0ad03ec62d78dd8090c5572daa9e65017277e5
liangk12 <kenneth.liang12@gmail.com>
2015-03-15 21:57:39 -0400

    working on correlation,covariance - got data for AAPL,MSFT,GOOG ticker values off Yahoo from 1/2010 to 1/2015

--- PythonForDataAnalysis/chapter5/pandasintro/essentialfunctionality/correlationcovariance.py
+++ PythonForDataAnalysis/chapter5/pandasintro/essentialfunctionality/correlationcovariance.py
@@ -6,3 +6,17 @@ import numpy as np
 import pandas as pd
 import pandas.io.data as web
 
+alldata={}
+for ticker in ["AAPL","MSFT","GOOG"]:
+    alldata[ticker]=web.get_data_yahoo(
+        ticker,"1/1/2010","1/1/2015"
+    )
+price=pd.DataFrame(
+    {tic:data["Adj Close"]
+     for tic,data in alldata.iteritems()}
+) # [1258 rows x 3 columns]
+volume=pd.DataFrame(
+    {tic:data["Volume"]
+     for tic,data in alldata.iteritems()}
+) # [1258 rows x 3 columns]
+
