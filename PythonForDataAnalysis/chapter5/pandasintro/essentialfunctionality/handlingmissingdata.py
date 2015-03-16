8dd953d0cfffb8b3a8d0fc35885140f8acbfe8a1
liangk12 <kenneth.liang12@gmail.com>
2015-03-16 00:13:07 -0400

    added Table5-12 NA handling methods, w addt'l examples of .dropna, .fillna

--- PythonForDataAnalysis/chapter5/pandasintro/essentialfunctionality/handlingmissingdata.py
+++ PythonForDataAnalysis/chapter5/pandasintro/essentialfunctionality/handlingmissingdata.py
@@ -1,5 +1,5 @@
 """
-NaN: missing data default value
+NaN: missing data default value (== None value)
      -a sentinel for easy detection
      -aka: flag value, signal value
      >sentinel: http://en.wikipedia.org/wiki/Sentinel_value
@@ -11,3 +11,48 @@ NaN: missing data default value
 import numpy as np
 import pandas as pd
 
+stringdata=pd.Series(
+    ["aardvark","artichoke",np.nan,"avocado"]
+)
+stringdata
+# 0     aardvark
+# 1    artichoke
+# 2          NaN
+# 3      avocado
+# dtype: object
+stringdata.isnull()
+# 0    False
+# 1    False
+# 2     True
+# 3    False
+# dtype: bool
+stringdata[0]=None # == np.nan (NaN)
+stringdata
+# 0         None
+# 1    artichoke
+# 2          NaN
+# 3      avocado
+# dtype: object
+stringdata.isnull()
+# 0     True
+# 1    False
+# 2     True
+# 3    False
+# dtype: bool
+stringdata.dropna()
+# 1    artichoke
+# 3      avocado
+# dtype: object
+stringdata.fillna("shoop")
+# 0        shoop
+# 1    artichoke
+# 2        shoop
+# 3      avocado
+# dtype: object
+
+
+# # Table5-12: NaN handling methods
+{'dropna': 'Filter axis labels based on whether values for each label have missing data, with varying thresholds for how much missing data to tolerate',
+ 'fillna': "Fill in missing data with some value or using an interpolation method such as 'ffill' or 'bfill'",
+ 'isnull': 'Return like-type object containing boolean values indicating which values are missing / NA',
+ 'notnull': 'Negation of isnull'}
