a78c213695f237b8f5923564d5129729d14cda73
liangk12 <kenneth.liang12@gmail.com>
2015-03-17 00:28:41 -0400

    went thru filtering missing data section with .dropna and its thresh arg for required count non-NaN values. Also reminder of slicing of DataFrame to get rows of col

--- PythonForDataAnalysis/chapter5/pandasintro/essentialfunctionality/filterout_missingdata.py
+++ PythonForDataAnalysis/chapter5/pandasintro/essentialfunctionality/filterout_missingdata.py
@@ -1,6 +1,8 @@
 """
 .dropna: return only non-null data, index values
          -default drops rows, cols that are NA
+
+: thresh arg requires that many non-NaN values
 """
 
 import numpy as np
@@ -46,6 +48,7 @@ data.dropna()
 data.dropna(how="all")
 # axis=0 default,
 # drops only entire rows that are NaN
+# per column
 '''
     0    1   2
 0   1  6.5   3
@@ -63,10 +66,75 @@ data[4]=NA;data # create entire col '4' is NaN
 data.dropna(axis=1,how="all")
 # axis=1,
 # drop entire cols that are NaN (col 4)
+# per row
 '''
     0    1   2
 0   1  6.5   3
 1   1  NaN NaN
 2 NaN  NaN NaN
 3 NaN  6.5   3
+'''
+
+# # filtering out DataFrame rows with
+# # concerns to time series data
+df=pd.DataFrame(np.random.randn(7,3))
+df
+'''
+          0         1         2
+0  0.169538 -1.596047 -0.150498
+1  1.325532  1.162959  0.606870
+2  0.804475  0.768876  0.408387
+3  1.555792 -0.211204 -1.693698
+4 -0.476087  0.568910 -0.625268
+5  0.814673  0.768167  1.029957
+6  1.277116 -0.697095 -0.134517
+'''
+df.ix[:4,1]=NA; df.ix[:2,2]=NA; df
+# syntax: df.ix[rowXi : rowXn, colIdx]
+# replace col 1, rows 0:4 = NA
+# replace col 2, rows 0:2 = NA
+'''
+          0         1         2
+0  0.169538       NaN       NaN
+1  1.325532       NaN       NaN
+2  0.804475       NaN       NaN
+3  1.555792       NaN -1.693698
+4 -0.476087       NaN -0.625268
+5  0.814673  0.768167  1.029957
+6  1.277116 -0.697095 -0.134517
+'''
+# # reminder: if df.ix[:n], get n rows of all cols
+df.ix[:3]
+'''
+          0   1         2
+0  0.169538 NaN       NaN
+1  1.325532 NaN       NaN
+2  0.804475 NaN       NaN
+3  1.555792 NaN -1.693698
+'''
+df.dropna()
+# drop all rows,cols w NaN value
+'''
+          0         1         2
+5  0.814673  0.768167  1.029957
+6  1.277116 -0.697095 -0.134517
+'''
+# **note: df.dropna(how="all")
+#         for either axis 0,1 pointless as
+#         not entire rows or cols as NaN
+df.dropna(thresh=3)
+# thresh arg requires that many non-NaN values
+# : requires row w 3 cols w non-NaN values
+'''
+          0         1         2
+5  0.814673  0.768167  1.029957
+6  1.277116 -0.697095 -0.134517
+'''
+df.dropna(thresh=2)
+'''
+          0         1         2
+3  1.555792       NaN -1.693698
+4 -0.476087       NaN -0.625268
+5  0.814673  0.768167  1.029957
+6  1.277116 -0.697095 -0.134517
 '''
