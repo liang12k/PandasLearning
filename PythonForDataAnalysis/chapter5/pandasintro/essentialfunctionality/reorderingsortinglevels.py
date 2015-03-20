76d4258e6c43b9834ea2184ad65e58bc091e0a5c
liangk12 <kenneth.liang12@gmail.com>
2015-03-20 00:32:59 -0400

    went thru reordering and sorting levels section. Still  tinkering a bit on .sort_index. Comments left around for .swaplevel, .sortlevel

--- PythonForDataAnalysis/chapter5/pandasintro/essentialfunctionality/reorderingsortinglevels.py
+++ PythonForDataAnalysis/chapter5/pandasintro/essentialfunctionality/reorderingsortinglevels.py
@@ -1,8 +1,83 @@
 """
+.swaplevel: takes two level numbers or names and
+            returns a new object with levels
+            interchanged (unaltered data)
 
+.sortlevel: sorts data using only values in a
+            single level
+            lexicographically sorted, use on
+            .swaplevel data
 """
 
 import numpy as np
 from numpy import nan as NA
 import pandas as pd
+from hierarchicalindexing import frame
 
+frame
+'''
+state      Ohio     Colorado
+color     Green Red    Green
+key1 key2
+a    1        0   1        2
+     2        3   4        5
+b    1        6   7        8
+     2        9  10       11
+'''
+frame.swaplevel("key1","key2")
+# == frame.swaplevel(0,1)
+'''
+state      Ohio     Colorado
+color     Green Red    Green
+key2 key1
+1    a        0   1        2
+2    a        3   4        5
+1    b        6   7        8
+2    b        9  10       11
+'''
+frame.swaplevel("key1","key2")==frame.swaplevel(0,1)
+'''
+state      Ohio       Colorado
+color     Green   Red    Green
+key2 key1
+1    a     True  True     True
+2    a     True  True     True
+1    b     True  True     True
+2    b     True  True     True
+'''
+frame.sortlevel(1)
+# 0 level = "a","b"
+# 1 level = 1,2
+'''
+state      Ohio     Colorado
+color     Green Red    Green
+key1 key2
+a    1        0   1        2
+b    1        6   7        8
+a    2        3   4        5
+b    2        9  10       11
+'''
+frame.swaplevel(0,1).sortlevel(0)
+# sort col 0 'key2'
+'''
+state      Ohio     Colorado
+color     Green Red    Green
+key2 key1
+1    a        0   1        2
+     b        6   7        8
+2    a        3   4        5
+     b        9  10       11
+'''
+frame.swaplevel(0,1).sortlevel(1)
+# sort col 1 'key1'
+# **note: all 'key2' values displayed
+#         to stick with 'key1' values
+'''
+state      Ohio     Colorado
+color     Green Red    Green
+key2 key1
+1    a        0   1        2
+2    a        3   4        5
+1    b        6   7        8
+2    b        9  10       11
+'''
