e5eb69212be1df43b056bc7dc12612d13badf49c
liangk12 <kenneth.liang12@gmail.com>
2015-03-22 23:25:20 -0400

    converted table6-1 pandas parsing funcs into dict

---
+++ PythonForDataAnalysis/chapter6/readwritetextformat.py
@@ -0,0 +1,10 @@
+"""
+
+"""
+
+# # table 6-1: pandas parsing functions
+{'read_clipboard': 'Version of read_table that reads data from the clipboard. Useful for converting tables from web pages',
+ 'read_csv': 'Load delimited data from a file, URL, or file-like object. Use comma as default delimiter',
+ 'read_fwf': 'Read data in fixed-width column format (that is, no delimiters)',
+ 'read_table': "Load delimited data from a file, URL, or file-like object. Use tab ('\t') as default delimiter"}
+
