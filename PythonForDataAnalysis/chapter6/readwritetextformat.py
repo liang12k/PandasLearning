915622c5cdd2058ef969e4a0c09ec8fba2c42461
liangk12 <kenneth.liang12@gmail.com>
2015-03-23 22:32:17 -0400

    working on ch6 readwrite text files. Typed out the intro and loaded ch6 ex1.csv. ditto'ed the book's ch6 example files into chapter6 dir

--- PythonForDataAnalysis/chapter6/readwritetextformat.py
+++ PythonForDataAnalysis/chapter6/readwritetextformat.py
@@ -1,5 +1,5 @@
 """
-
+convert text datat into a DataFrame
 """
 
 # # table 6-1: pandas parsing functions
@@ -8,3 +8,35 @@
  'read_fwf': 'Read data in fixed-width column format (that is, no delimiters)',
  'read_table': "Load delimited data from a file, URL, or file-like object. Use tab ('\t') as default delimiter"}
 
+'''
+Indexing: 
+treat 1+ cols as the returned DataFrame
+col names from file, user, or not at all
+
+Type inference, data conversion:
+user-defined value conversions and 
+custom list of missing value markers
+
+Datetime parsing: 
+combining capability - date,time information
+spread over multiple cols into single col in result
+
+Iterating:
+iterating support over chunks of very large files
+
+Unclean data issues:
+skipping content (rows, footer, comments, etc)
+'''
+
+import pandas as pd
+
+# # Type inference
+# # no need to specify col types
+df=pd.read_csv("ex1.csv")
+df
+'''
+   a   b   c   d message
+0  1   2   3   4   hello
+1  5   6   7   8   world
+2  9  10  11  12     foo
+'''
