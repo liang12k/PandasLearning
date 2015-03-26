b7b6004150bb0d89aa91de696331adac2e784bbf
liangk12 <kenneth.liang12@gmail.com>
2015-03-25 22:58:53 -0400

    created method to dynamically get mockdata from module using getattr and checking if obj is a dict using isinstance

--- PythonForDataAnalysis/chapter6/misc/mockdatadataframe.py
+++ PythonForDataAnalysis/chapter6/misc/mockdatadataframe.py
@@ -5,17 +5,25 @@ import  googleplus_mockdata as googledata
 import twitter_mockdata as twitterdata
 import pandas as pd
 
+def getMockDataVariables(inpModule):
+    """
+        :return list: list of module's attributes (variable) values
+    """
+    return [
+        getattr(inpModule,data)
+        for data in dir(inpModule)
+        if ("__" not in data
+            and isinstance(
+                    getattr(inpModule,data), dict)
+        ) # end if
+    ]
+
 googleDf=pd.DataFrame(
-    [googledata.activity0,
-     googledata.activity1,
-     googledata.activity2,
-     googledata.activity3]
+    getMockDataVariables(googledata)
 )
 googleDf
 
 twitterDf=pd.DataFrame(
-    [twitterdata.status0,
-     twitterdata.status1,
-     twitterdata.status2]
+    getMockDataVariables(twitterdata)
 )
 twitterDf
