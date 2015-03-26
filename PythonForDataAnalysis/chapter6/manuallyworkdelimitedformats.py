c8c427e8bab0c8d2e918f9a2db3e2ddd8d6eced4
liangk12 <kenneth.liang12@gmail.com>
2015-03-26 00:03:20 -0400

    header info for pd.read_table, csv for 'manually working with delimited formats' section/module

---
+++ PythonForDataAnalysis/chapter6/manuallyworkdelimitedformats.py
@@ -0,0 +1,11 @@
+"""
+pandas.read_table
+-load tabular data from disk, generally
+
+csv.reader
+-built in Python module
+-handles files with single character delimiter
+"""
+
+import csv
+import pandas as pd
