ad403c3f96f75b1210c059c48b43320128a20f81
liangk12 <kenneth.liang12@gmail.com>
2015-03-26 21:58:30 -0400

    some wrangling applied onto ex7.csv to get a datadict of file with header and lines

--- PythonForDataAnalysis/chapter6/manuallyworkdelimitedformats.py
+++ PythonForDataAnalysis/chapter6/manuallyworkdelimitedformats.py
@@ -9,3 +9,23 @@ csv.reader
 
 import csv
 import pandas as pd
+
+list(open("ex7.csv"))
+# ['"a","b","c"\n', '"1","2","3"\n', '"1","2","3","4"\n']
+f=open("ex7.csv")
+reader=csv.reader(f)
+for line in reader:
+    print line
+'''
+['a', 'b', 'c']
+['1', '2', '3']
+['1', '2', '3', '4']
+'''
+# wrangling needed, missing a col for row index 1
+lines=list(csv.reader(open("ex7.csv")))
+header,values = lines[0],lines[1:]
+datadict={h:v for h,v in zip(header,zip(*values))}
+# # note: zip(*values) applies zip onto values itself
+# [('1', '1'), ('2', '2'), ('3', '3')]
+datadict
+# {'a': ('1', '1'), 'b': ('2', '2'), 'c': ('3', '3')}
