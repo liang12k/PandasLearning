d8ed8ad72d24851f9e6ead0b366f9f59f59f3849
liangk12 <kenneth.liang12@gmail.com>
2015-04-08 22:55:41 -0400

    create sqlite3 db and committed a small dataset into table. Printed out the same data using a query and .fetchall to see dataset

--- PythonForDataAnalysis/chapter6/interactingwithdatabases.py
+++ PythonForDataAnalysis/chapter6/interactingwithdatabases.py
@@ -16,4 +16,23 @@ query="""
 CREATE TABLE test
 (a VARCHAR(20), b VARCHAR(20), c REAL, d INTEGER);
 """
+con=sqlite3.connect(":memory:")
+# print con # <sqlite3.Connection at 0x1046823d0>
+con.execute(query)
+con.commit()
 
+data=[
+    ("Atlanta","Georgia",1.25,6),
+    ("Tallahassee","Florida",2.6,3),
+    ("Sacramento","California",1.7,5)
+]
+stmt="INSERT INTO test VALUES(?,?,?,?)"
+con.executemany(stmt,data)
+con.commit()
+
+cursor=con.execute("SELECT * FROM test")
+rows=cursor.fetchall()
+print rows
+"""
+[(u'Atlanta', u'Georgia', 1.25, 6), (u'Tallahassee', u'Florida', 2.6, 3), (u'Sacramento', u'California', 1.7, 5)]
+"""
