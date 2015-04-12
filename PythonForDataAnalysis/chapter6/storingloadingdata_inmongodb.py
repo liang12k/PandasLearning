80bcdffdc9e5e34cde16d11b828120d20949aa7e
liangk12 <kenneth.liang12@gmail.com>
2015-04-12 19:54:57 -0400

    working with the mongodb saving the colorado businesses data from interactingwith_htmlandwebapis.py url. Getting ServerSelectionTimeoutError -- looking into it

--- PythonForDataAnalysis/chapter6/storingloadingdata_inmongodb.py
+++ PythonForDataAnalysis/chapter6/storingloadingdata_inmongodb.py
@@ -5,6 +5,7 @@
 import pymongo
 import requests
 import json
+from interactingwith_htmlandwebapis import url
 
 # no such attribute 'Connection' (from the book)
 # conn=pymongo.Connection("localhost",port=808080)
@@ -13,3 +14,7 @@ import json
 conn=pymongo.MongoClient("localhost",808080)
 print conn
 # MongoClient('localhost', 808080)
+
+coloradoBusinesses=conn.db.coloradoBusinesses
+data=json.loads(requests.get(url).text)
+[coloradoBusinesses.save(d) for d in data]
