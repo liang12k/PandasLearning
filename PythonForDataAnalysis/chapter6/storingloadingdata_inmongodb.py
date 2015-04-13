7f46ea7f6bb2868e054e8121f652ebf8be371977
liangk12 <kenneth.liang12@gmail.com>
2015-04-12 22:32:51 -0400

    installed mongodb using hombrew following instructions from docs.mongodb.org. Created a local db in this dir and default MongoClient setup. Operated on the sample json site of Colorado real estate data

--- PythonForDataAnalysis/chapter6/storingloadingdata_inmongodb.py
+++ PythonForDataAnalysis/chapter6/storingloadingdata_inmongodb.py
@@ -1,7 +1,10 @@
 """
-
+to get mongodb working, be sure to install mongodb!
+follow instructions here:
+http://docs.mongodb.org/manual/tutorial/install-mongodb-on-os-x/
 """
 
+import pandas as pd
 import pymongo
 import requests
 import json
@@ -11,10 +14,42 @@ from interactingwith_htmlandwebapis import url
 # conn=pymongo.Connection("localhost",port=808080)
 # from documentation:
 # http://api.mongodb.org/python/current/tutorial.html
-conn=pymongo.MongoClient("localhost",808080)
-print conn
-# MongoClient('localhost', 808080)
+conn=pymongo.MongoClient()
+print conn.test_database
+# Database(MongoClient('localhost', 27017), u'test_database')
 
 coloradoBusinesses=conn.db.coloradoBusinesses
 data=json.loads(requests.get(url).text)
 [coloradoBusinesses.save(d) for d in data]
+
+cursor=coloradoBusinesses.find(
+    {
+        "agentprincipalzipcode":"81303"
+    }
+)
+coBusin_fields=[
+    "agentfirstname",
+    "agentlastname",
+    "agentprincipalcity",
+    "agentprincipaladdress1",
+    "agentprincipalzipcode"
+]
+result=pd.DataFrame(
+    list(cursor),
+    columns=coBusin_fields
+)
+'''
+  agentfirstname agentlastname agentprincipalcity  agentprincipaladdress1  \
+0           Rani          Holt            Durango  250 Morning Glory Lane
+1           Rani          Holt            Durango  250 Morning Glory Lane
+2           Rani          Holt            Durango  250 Morning Glory Lane
+3           Rani          Holt            Durango  250 Morning Glory Lane
+4           Rani          Holt            Durango  250 Morning Glory Lane
+
+  agentprincipalzipcode
+0                 81303
+1                 81303
+2                 81303
+3                 81303
+4                 81303
+'''
