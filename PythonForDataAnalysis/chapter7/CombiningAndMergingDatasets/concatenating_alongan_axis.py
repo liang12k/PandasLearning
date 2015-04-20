cdae6e5b089f3db9971926fa0972a68691c6d0fa
liangk12 <kenneth.liang12@gmail.com>
2015-04-19 21:22:26 -0400

    addt'l samples on pd.concat using args join_axes, keys for hierarchical index

--- PythonForDataAnalysis/chapter7/CombiningAndMergingDatasets/concatenating_alongan_axis.py
+++ PythonForDataAnalysis/chapter7/CombiningAndMergingDatasets/concatenating_alongan_axis.py
@@ -65,9 +65,77 @@ b   1  5
 f NaN  5
 g NaN  6
 '''
+# print pd.concat([s1,s4])
+# defaults to axis=0 (rows)
+# -**notice the duplicate index values
+'''
+a    0
+b    1
+a    0
+b    5
+f    5
+g    6
+dtype: int64
+'''
 # print pd.concat([s1,s4],axis=1,join="inner")
+# only on same index values
 '''
    0  1
 a  0  0
 b  1  5
+'''
+# print pd.concat([s1,s4],axis=1,join_axes=[list("acbe")])
+# specify axes to join (in this case, axis=1, cols)
+'''
+    0   1
+a   0   0
+c NaN NaN
+b   1   5
+e NaN NaN
+'''
+# # hierarchical index on concat axis
+result=pd.concat([s1,s1,s3],keys=["one","two","three"])
+# print s1
+'''
+a    0
+b    1
+dtype: int64
+'''
+# print s3
+'''
+f    5
+g    6
+dtype: int64
+'''
+# print result
+# allots keys for each DataFrame
+'''
+one    a    0
+       b    1
+two    a    0
+       b    1
+three  f    5
+       g    6
+dtype: int64
+'''
+# print result.unstack()
+# converts result back to 2D DataFrame
+'''
+        a   b   f   g
+one     0   1 NaN NaN
+two     0   1 NaN NaN
+three NaN NaN   5   6
+'''
+# print pd.concat([s1,s2,s4],keys=["one","two","three"])
+'''
+one    a    0
+       b    1
+two    c    2
+       d    3
+       e    4
+three  a    0
+       b    5
+       f    5
+       g    6
+dtype: int64
 '''
